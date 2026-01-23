8bde214f1b312a8aaca279ff0e67dc08e0ce62ff
pdarleyjr <pdarleyjr@gmail.com>
2026-01-23 10:19:45 -0500

    Fix /daily React SPA base path and rebuild assets

--- resources/js/daily-checkout/vite.config.js
+++ resources/js/daily-checkout/vite.config.js
@@ -4,6 +4,7 @@ import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/daily/',
   plugins: [
     react(),
     sentryVitePlugin({
