ed448bd9fe0a15a6359fe5b42ddf6acc3c2ed1b1
pdarleyjr <pdarleyjr@gmail.com>
2026-01-24 18:39:45 -0500

    feat: add User seeder for initial accounts

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = [
+            ['email' => 'PeterDarley@miamibeachfl.gov', 'name' => 'Peter Darley', 'password' => Hash::make('Penco3')],
+            ['email' => 'RichardQuintela@miamibeachfl.gov', 'name' => 'Richard Quintela', 'password' => Hash::make('Penco2')],
+            ['email' => 'MiguelAnchia@miamibeachfl.gov', 'name' => 'Miguel Anchia', 'password' => Hash::make('Admin123')],
+            ['email' => 'geralddeyoung@miamibeachfl.gov', 'name' => 'Gerald DeYoung', 'password' => Hash::make('MBFDGerry1')],
+        ];
+
+        foreach ($users as $userData) {
+            User::updateOrCreate(
+                ['email' => $userData['email']],
+                $userData
+            );
+        }
+        
+        $this->command->info('Users created successfully!');
+    }
+}
