6cd8662d83aa472c2e0545ee19530880d69e371a
pdarleyjr <pdarleyjr@gmail.com>
2026-01-25 16:04:06 -0500

    fix(todos): resolve class not found error in TodoResource

--- app/Filament/Resources/TodoResource/Pages/ListTodos.php
+++ app/Filament/Resources/TodoResource/Pages/ListTodos.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\TodoResource\Pages;
 use App\Filament\Resources\TodoResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTodos extends ListRecords
 {
@@ -14,6 +16,17 @@ class ListTodos extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All'),
+            'assigned_to_me' => Tab::make('Assigned to me')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('assigned_to', auth()->id())),
+            'created_by_me' => Tab::make('Created by me')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_by', auth()->id())),
         ];
     }
 }
