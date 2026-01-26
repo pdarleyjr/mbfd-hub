cada89e61dbe993488a1c99e9d591dd293b447a3
pdarleyjr <pdarleyjr@gmail.com>
2026-01-25 23:03:23 -0500

    Add mobile UI enhancements - collapsible sidebar, iOS/Android app-like experience

--- laravel-app/app/Providers/Filament/AdminPanelProvider.php
+++ laravel-app/app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Inter')
             ->sidebarCollapsibleOnDesktop()
+            ->sidebarFullyCollapsibleOnDesktop()
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
