1ee7c49ade13237ace5982bd3ddf9850dbfb4809
pdarleyjr <pdarleyjr@gmail.com>
2026-01-25 22:33:02 -0500

    Add collapsible sidebar on desktop

--- laravel-app/app/Providers/Filament/AdminPanelProvider.php
+++ laravel-app/app/Providers/Filament/AdminPanelProvider.php
@@ -39,6 +39,7 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Amber,
             ])
             ->font('Inter')
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
