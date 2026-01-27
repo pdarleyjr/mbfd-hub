5a7976b06b6f372ac803af24665aeb21b268f421
pdarleyjr <pdarleyjr@gmail.com>
2026-01-26 19:58:15 -0500

    Fix: Move PushNotificationWidget JS to external file to prevent Filament stripping

--- app/Filament/Widgets/PushNotificationWidget.php
+++ app/Filament/Widgets/PushNotificationWidget.php
@@ -13,6 +13,12 @@ class PushNotificationWidget extends Widget
     
     protected static ?int $sort = 1;
 
+    protected static array $assets = [
+        'js' => [
+            'js/push-notification-widget.js',
+        ],
+    ];
+
     public function getVapidPublicKey(): string
     {
         return config('webpush.vapid.public_key', '');
