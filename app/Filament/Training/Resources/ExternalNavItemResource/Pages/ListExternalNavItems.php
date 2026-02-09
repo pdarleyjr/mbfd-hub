14bdc2cf9fb4e4bac82d16fa8ae33a4006e33333
pdarleyjr <pdarleyjr@gmail.com>
2026-02-09 08:24:11 -0500

    Fix training user credentials, add Baserow navigation link and quick-access button

---
+++ app/Filament/Training/Resources/ExternalNavItemResource/Pages/ListExternalNavItems.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Training\Resources\ExternalNavItemResource\Pages;
+
+use App\Filament\Training\Resources\ExternalNavItemResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListExternalNavItems extends ListRecords
+{
+    protected static string $resource = ExternalNavItemResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('open_baserow')
+                ->label('Open Baserow')
+                ->icon('heroicon-o-arrow-top-right-on-square')
+                ->url('https://baserow.support.darleyplex.com', shouldOpenInNewTab: true)
+                ->color('info'),
+            Actions\CreateAction::make(),
+        ];
+    }
+}
