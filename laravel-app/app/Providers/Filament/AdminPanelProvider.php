26abf2b26a287430d3962c1e0ea50aa930c8c452
Peter Darley <pdarleyjr@gmail.com>
2026-03-01 12:56:53 -0500

    Fix admin logo and remove viteTheme that was causing build errors

--- laravel-app/app/Providers/Filament/AdminPanelProvider.php
+++ laravel-app/app/Providers/Filament/AdminPanelProvider.php
@@ -44,7 +44,6 @@ class AdminPanelProvider extends PanelProvider
             ->font('Inter')
             ->sidebarCollapsibleOnDesktop()
             ->sidebarFullyCollapsibleOnDesktop()
-            ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
