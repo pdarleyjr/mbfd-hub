4e3a062d40c43bd74a4369b91b1544adbc9599b1
Peter Darley <pdarleyjr@gmail.com>
2026-03-02 16:14:00 -0500

    fix: Remove stray quotes from blade template

--- resources/views/filament-workgroup/pages/evaluation-form.blade.php
+++ resources/views/filament-workgroup/pages/evaluation-form.blade.php
@@ -1,19 +1,19 @@
-"<x-filament-panels::page>
+<x-filament-panels::page>
     <form wire:submit.prevent="submitEvaluation">
         {{ $this->form }}
 
         @if(!$isReadOnly)
             <div class="mt-6 flex flex-wrap gap-3">
                 <x-filament::button wire:click="setAllHighest" color="warning" type="button">
-                    ⭐ Set All to Highest
+                    Set All to Highest
                 </x-filament::button>
                 <x-filament::button wire:click="saveDraft" color="gray" type="button">
-                    💾 Save as Draft
+                    Save as Draft
                 </x-filament::button>
                 <x-filament::button wire:click="submitEvaluation" color="success" type="button">
-                    ✅ Submit Evaluation
+                    Submit Evaluation
                 </x-filament::button>
             </div>
         @endif
     </form>
-</x-filament-panels::page>" 
+</x-filament-panels::page>
