477eb76b8e89fa9236638c35d532c80a5b7ab3d4
Peter Darley <pdarleyjr@gmail.com>
2026-03-02 15:57:04 -0500

    fix: Major improvements to evaluation system

--- resources/views/filament-workgroup/pages/evaluation-form.blade.php
+++ resources/views/filament-workgroup/pages/evaluation-form.blade.php
@@ -3,12 +3,15 @@
         {{ $this->form }}
 
         @if(!$isReadOnly)
-            <div class="mt-6 flex gap-3">
+            <div class="mt-6 flex flex-wrap gap-3">
+                <x-filament::button wire:click="setAllHighest" color="warning" type="button">
+                    ⭐ Set All to Highest
+                </x-filament::button>
                 <x-filament::button wire:click="saveDraft" color="gray" type="button">
-                    Save as Draft
+                    💾 Save as Draft
                 </x-filament::button>
                 <x-filament::button wire:click="submitEvaluation" color="success" type="button">
-                    Submit Evaluation
+                    ✅ Submit Evaluation
                 </x-filament::button>
             </div>
         @endif
