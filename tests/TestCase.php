bdeeeb4661cfc7a3da3137083b86f824570ab432
pdarleyjr <pdarleyjr@gmail.com>
2026-08-26 19:02:21 -0400

    test: isolate phpunit from external integrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,16 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Process;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Http::preventStrayRequests();
+        Process::preventStrayProcesses();
+    }
 }
