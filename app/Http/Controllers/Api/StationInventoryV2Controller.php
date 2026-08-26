62e4749e89246eb62fce24012bbda2d87f135317
pdarleyjr <pdarleyjr@gmail.com>
2026-08-26 18:36:55 -0400

    fix: retain station inventory signed URL facade

--- app/Http/Controllers/Api/StationInventoryV2Controller.php
+++ app/Http/Controllers/Api/StationInventoryV2Controller.php
@@ -10,6 +10,7 @@ use App\Models\StationSupplyRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 
 /**
