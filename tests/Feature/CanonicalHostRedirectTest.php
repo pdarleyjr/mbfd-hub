962664c48e84e406143387110e09c3e2b5e43679
pdarleyjr <pdarleyjr@gmail.com>
2026-09-11 20:00:45 -0400

    Add canonical Daily host middleware regression

---
+++ tests/Feature/CanonicalHostRedirectTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+final class CanonicalHostRedirectTest extends TestCase
+{
+    public function test_daily_apex_requests_redirect_to_the_configured_canonical_origin_with_path_and_query(): void
+    {
+        config(['app.url' => 'https://www.mbfdhub.com']);
+
+        $this->withServerVariables(['HTTP_HOST' => 'mbfdhub.com'])
+            ->get('/daily/stations?foo=bar')
+            ->assertStatus(308)
+            ->assertHeader('Location', 'https://www.mbfdhub.com/daily/stations?foo=bar');
+    }
+
+    public function test_daily_requests_on_the_canonical_host_are_not_canonical_redirected(): void
+    {
+        config(['app.url' => 'https://www.mbfdhub.com']);
+
+        $response = $this->withServerVariables(['HTTP_HOST' => 'www.mbfdhub.com'])
+            ->get('/daily/stations?foo=bar');
+
+        $response->assertStatus(302);
+        $this->assertStringEndsWith('/login', (string) $response->headers->get('Location'));
+    }
+
+    public function test_localhost_remains_usable_for_daily_feature_tests(): void
+    {
+        config(['app.url' => 'https://www.mbfdhub.com']);
+
+        $this->withServerVariables(['HTTP_HOST' => 'localhost'])
+            ->get('/daily/stations?foo=bar')
+            ->assertRedirect('/login');
+    }
+}
